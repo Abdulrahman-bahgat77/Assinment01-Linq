e673be490bcac4050b72475aae2cabcd05117f2e
abdulrahman <abdulrahman403868@fci.bu.edu.eg>
2025-02-03 02:09:42 +0200

    Add project files.

---
+++ Assinment01-Linq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assinment01_Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
